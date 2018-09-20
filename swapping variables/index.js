1e848cb472e56766956b6d62249a7b3798897044
chillykirby1 <32395135+chillykirby1@users.noreply.github.com>
2018-09-20 13:15:39 -0600

    changes

--- swapping variables/index.js
+++ swapping variables/index.js
@@ -1,16 +1,8 @@
-console.log('hello world');
-
-// create 10 variables about yourself
 let firstname = "kaleb";
 let lastname = "griffiths";
-let age = "15";
-const ageAfraid = 29; // security feature
-let hair;
-let male = true;
+
+// new code goes here
 
 // print variables to console
 console.log(firstname);
 console.log(lastname);
-console.log(15);
-console.log(some);
-console.log("am i male? " + male);
