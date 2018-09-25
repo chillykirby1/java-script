39b926b747b48cde4b9dfabe6615c50143d72945
chillykirby1 <32395135+chillykirby1@users.noreply.github.com>
2018-09-25 13:54:26 -0600

    update

---
+++ function arrays assignment/index.js
@@ -0,0 +1,18 @@
+console.log("Using array-data-strucutre, write a function(s) to switch the content of both arrays in the second element ... the last name.")
+
+// Your Function Goes here
+
+
+let kalebGriffiths = [];
+let MeganSpence = [];
+
+// firstName, lastName, age, ageAfraid, hair, male, female
+kalebGriffiths = ["kaleb", "Griffiths", 13, 29, null, true, false];
+MeganSpence = ["Megan", "Spence", 14, null, true, false, true];
+
+//Your code goes here
+
+
+// Print first names only
+console.log("Last Name of kalebGriffiths is: " + kalebGriffiths[1]); //Should return Spence
+console.log("Last Name of MeganSpence is: " + MeganSpence[1]); //Should return Mercer
