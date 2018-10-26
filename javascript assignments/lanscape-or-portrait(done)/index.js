4225897767e9e10a1804f00e875e6b3a9d290eef
chillykirby1 <32395135+chillykirby1@users.noreply.github.com>
2018-10-26 12:46:39 -0600

    the biggest change ever

---
+++ javascript assignments/lanscape-or-portrait(done)/index.js
@@ -0,0 +1,11 @@
+let width = 5;
+let height = 4;
+
+if (width == height)
+ console.log("square");
+
+if (width > height)
+ console.log ("landscape");
+
+if (width < height)
+ console.log ("portrait");
--- javascript assignments/ordering-grades(done)/index.js
+++ javascript assignments/ordering-grades(done)/index.js
@@ -1,13 +1,13 @@
 //Variable is an array for security
-const marks = []; //add grades as an array
+const marks = [56, 89, 72]; //add grades as an array
 
 //Use the calculatedGrade function as an output to the console
 //Note: example of using functions more than once (calculateAverage is used in console.log and calculatedGrade)
-console.log ("My letter grade is", calculatedGrade (average), "because my numerical average is", calculateAverage (marks));
+console.log ("My letter grade is", calculatedGrade (marks), "because my numerical average is", calculateAverage (marks));
 
 //This function uses another function, call Single Concern Function (able to use either function for various purposes)
-function calculatedGrade (marks) {
-  const average = calculateAverage (marks);
+function calculatedGrade (grades) {
+  const average = calculateAverage (grades);
 
   if (79 <= average)
   if (averge < 89)
@@ -27,8 +27,8 @@ function calculatedGrade (marks) {
 
   return 'A';
 }
-function calculteAverage (array) {
+function calculateAverage (arrays) {
   let sum = 0;
-  for (let mark of marks) sum += mark; //Example of reading an array and one line to add up the sum of all the grades
+  for (let array of arrays) sum += array; //Example of reading an array and one line to add up the sum of all the grades
   return sum / marks.length; //"Divide the total marks by the number of grades"
 }
